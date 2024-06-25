57b9b2037bf85c320c8c67e2613dfbed1f971a98
oMaDaSa <matheusdsantana07@gmail.com>
2024-06-24 21:22:26 -0300

    Changed Application class name

--- src/main/java/dio/MovieLog/Application.java
+++ src/main/java/dio/MovieLog/Application.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
-public class MovieLogApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieLogApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
