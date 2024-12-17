a3a93158038dc6178cc9119956942f420b3d82c0
oMaDaSa <matheusdsantana07@gmail.com>
2024-12-17 11:53:42 -0300

    autoconfigure exclude

--- src/main/java/dio/MovieLog/Application.java
+++ src/main/java/dio/MovieLog/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
+
 public class Application {
 
 	public static void main(String[] args) {
